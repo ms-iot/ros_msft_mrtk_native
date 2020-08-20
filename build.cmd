43429d3ee2868c1344c7293124322b1f3d6a093a
Lou Amadio <lamadio@microsoft.com>
2020-08-20 16:46:40 -0700

    build fixes

--- build.cmd
+++ build.cmd
@@ -2,26 +2,22 @@
 setlocal enableextensions
 
 : Call to build the isolated ROS2 build system
+set ChocolateyInstall=c:\opt\chocolatey
+set PATH=c:\opt\vcpkg\installed\x64-windows\bin;c:\opt\vcpkg;c:\opt\chocolatey\bin;C:\opt\python37amd64\;C:\opt\python37amd64\Scripts;C:\opt\python37amd64\DLLs;%PATH%
 
-set "CMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH:;C:/opt/vcpkg/installed/x64-uwp=%"
 set "CMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH%;C:/opt/vcpkg/installed/x64-uwp"
 set "VCPKG_ROOT=c:\opt\vcpkg"
 set "PATH=%PATH:c:\opt\vcpkg;c:\opt\vcpkg\installed\x64-uwp\bin;=%"
 set "PATH=%VCPKG_ROOT%;%VCPKG_ROOT%\installed\x64-uwp\bin;%PATH%"
-set ChocolateyInstall=c:\opt\chocolatey 
 
-vcpkg install opencv4:x64-uwp
-vcpkg install apriltag:x64-uwp
-vcpkg install eigen3:x64-uwp
+call tools\install\local_setup.bat
 
-cd tools
-call colcon build --merge-install --cmake-args -DBUILD_TESTING=OFF
-call install\local_setup.bat
+set ROS2_ARCH=x64
+cd target
+//call colcon build --merge-install --packages-ignore tf2_py examples_tf2_py rmw_fastrtps_dynamic_cpp rcl_logging_log4cxx rclcpp_components ros2trace tracetools_launch tracetools_read tracetools_test tracetools_trace --cmake-args -A x64 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DRMW_IMPLEMENTATION=rmw_fastrtps_cpp -DTHIRDPARTY=ON -DINSTALL_EXAMPLES=OFF -DBUILD_TESTING=OFF
+call colcon build --merge-install --packages-ignore tf2_py examples_tf2_py rmw_fastrtps_dynamic_cpp rcl_logging_log4cxx rcl_logging_spdlog rclcpp_components ros2trace tracetools_launch tracetools_read tracetools_test tracetools_trace --cmake-args -A %ROS2_ARCH% -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DTHIRDPARTY=ON -DINSTALL_EXAMPLES=OFF -DBUILD_TESTING=OFF -DRCL_LOGGING_IMPLEMENTATION=rcl_logging_noop
 
-cd ..\target
-call colcon build --merge-install --packages-ignore tf2_py examples_tf2_py rmw_fastrtps_dynamic_cpp rcl_logging_log4cxx rclcpp_components ros2trace tracetools_launch tracetools_read tracetools_test tracetools_trace --cmake-args -DRMW_IMPLEMENTATION=rmw_fastrtps_cpp -DTHIRDPARTY=ON -DINSTALL_EXAMPLES=OFF -DBUILD_TESTING=OFF
-
-ren target\install x64
+// ren target\install x64
 
 echo Success
 exit /b 0
--- init.cmd
+++ init.cmd
@@ -3,10 +3,14 @@ setlocal enableextensions disabledelayedexpansion
 
 : Call to initialize the isolated ROS2 build system
 
-set "ChocolateyInstall=c:\opt\chocolatey"
-set "PATH=c:\opt\chocolatey\bin;C:\opt\python37amd64\;C:\opt\python37amd64\Scripts;%PATH%"
-set PATH=C:\opt\python37amd64\DLLs;%PATH%
-set CMAKE_PREFIX_PATH=C:\opt\rosdeps\x64\include;%CMAKE_PREFIX_PATH%
+set ChocolateyInstall=c:\opt\chocolatey
+set PATH=c:\opt\vcpkg\installed\x64-windows\bin;c:\opt\vcpkg;c:\opt\chocolatey\bin;C:\opt\python37amd64\;C:\opt\python37amd64\Scripts;C:\opt\python37amd64\DLLs;%PATH%
+
+set "CMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH:;C:/opt/vcpkg/installed/x64-uwp=%"
+set "CMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH%;C:/opt/vcpkg/installed/x64-uwp"
+set "VCPKG_ROOT=c:\opt\vcpkg"
+set "PATH=%PATH:c:\opt\vcpkg;c:\opt\vcpkg\installed\x64-uwp\bin;=%"
+set "PATH=%VCPKG_ROOT%;%VCPKG_ROOT%\installed\x64-uwp\bin;%PATH%"
 
 
 mkdir tools\src
@@ -20,3 +24,16 @@ vcs import src < ..\ros2_uwp.repos
 
 xcopy /y src\ros2\orocos_kinematics_dynamics\orocos_kdl\config\FindEigen3.cmake src\ros2\eigen3_cmake_module\cmake\Modules
 set CMAKE_PREFIX_PATH=C:\opt\rosdeps\x64\include\eigen3;%CMAKE_PREFIX_PATH%
+
+
+vcpkg install protobuf:x64-windows
+vcpkg install protobuf:x64-uwp
+vcpkg install opencv4:x64-uwp
+vcpkg install apriltag:x64-uwp
+vcpkg install eigen3:x64-uwp
+
+cd tools
+call colcon build --merge-install --cmake-args -DBUILD_TESTING=OFF
+call install\local_setup.bat
+
+
