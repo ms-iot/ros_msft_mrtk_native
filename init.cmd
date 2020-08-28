78c26eb6126728074c157f6647434b26a9672b4a
Lou Amadio <lamadio@microsoft.com>
2020-08-27 19:51:16 -0700

    iterate

--- init.cmd
+++ init.cmd
@@ -8,7 +8,7 @@ mkdir c:\opt\chocolatey
 set PYTHONHOME=C:\opt\python27amd64\
 set ChocolateyInstall=c:\opt\chocolatey
 choco source add -n=ros-win -s="https://aka.ms/ros/public" --priority=1
-choco upgrade ros_colcon_tools -y --execution-timeout=0 --pre
+choco upgrade ros-colcon-tools -y --execution-timeout=0 --pre
 
 : choco upgrade ros_vcpkg -y --execution-timeout=0 --pre
 : include staged vcpkgs
@@ -20,7 +20,7 @@ set VCPKG_ROOT=c:\opt\vcpkg
 mkdir c:\opt\vcpkg
 cd c:\opt\vcpkg
 git clone https://github.com/ooeygui/vcpkg
-call bootstrap_vcpkg.bat
+call bootstrap-vcpkg.bat
 
 pip install vcs
 
@@ -48,12 +48,15 @@ vcpkg install opencv4[core]:x64-uwp
 vcpkg install apriltag:x64-uwp
 vcpkg install eigen3:x64-uwp
 vcpkg install foonathan-memory[core]:x64-uwp
+vcpkg install poco:x64-uwp
 
+vcpkg install protobuf:x86-uwp
 vcpkg install asio:x86-uwp
 vcpkg install opencv4[core]:x86-uwp
 vcpkg install apriltag:x86-uwp
 vcpkg install eigen3:x86-uwp
 vcpkg install foonathan-memory:x86-uwp
+vcpkg install poco:x86-uwp
 
 vcpkg install protobuf:arm64-uwp
 vcpkg install asio:arm64-uwp
@@ -62,6 +65,7 @@ vcpkg install opencv4[core]:arm64-uwp
 vcpkg install apriltag:arm64-uwp
 vcpkg install eigen3:arm64-uwp
 vcpkg install foonathan-memory[core]:arm64-uwp
+vcpkg install poco:arm64-uwp
 
 cd tools
 call colcon build --merge-install --cmake-args -DBUILD_TESTING=OFF
