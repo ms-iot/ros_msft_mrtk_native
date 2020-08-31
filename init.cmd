afd4467b49a644809facb0a7c63581fc4ec6f452
Lou Amadio <lamadio@microsoft.com>
2020-08-31 15:39:12 -0700

    Add separate vcpkg build for speed

--- init.cmd
+++ init.cmd
@@ -1,6 +1,7 @@
 @echo off
 setlocal enableextensions disabledelayedexpansion
 
+if NOT EXIST "c:\opt\vcpkg\vcpkg.exe" goto :novcpkg
 
 
 : Call to initialize the isolated ROS2 build system
@@ -17,14 +18,7 @@ set PATH_ORIG=%PATH%
 set PATH=c:\opt\vcpkg;c:\opt\chocolatey\bin;C:\opt\python37amd64\;C:\opt\python37amd64\Scripts;C:\opt\python37amd64\DLLs;%PATH%
 set VCPKG_ROOT=c:\opt\vcpkg
 
-mkdir c:\opt
-pushd c:\opt
-git clone https://github.com/ooeygui/vcpkg
-call bootstrap-vcpkg.bat
-
 call pip install vcs
-popd
-
 
 mkdir tools\src
 mkdir target\src
@@ -38,39 +32,14 @@ cd ..
 
 xcopy /y src\ros2\orocos_kinematics_dynamics\orocos_kdl\config\FindEigen3.cmake src\ros2\eigen3_cmake_module\cmake\Modules
 
-: Build tooling
-vcpkg install protobuf:x86-windows
-vcpkg install foonathan-memory:x86-windows
-
-:build_arm64
-vcpkg install protobuf:arm64-uwp
-vcpkg install asio:arm64-uwp
-vcpkg install tinyxml2:arm64-uwp
-vcpkg install opencv4[core]:arm64-uwp
-vcpkg install apriltag:arm64-uwp
-vcpkg install eigen3:arm64-uwp
-vcpkg install foonathan-memory[core]:arm64-uwp
-vcpkg install poco:arm64-uwp
-
-:build_x64
-vcpkg install protobuf:x64-uwp
-vcpkg install asio:x64-uwp
-vcpkg install opencv4[core]:x64-uwp
-vcpkg install apriltag:x64-uwp
-vcpkg install eigen3:x64-uwp
-vcpkg install foonathan-memory[core]:x64-uwp
-vcpkg install poco:x64-uwp
-
-:build_x86
-vcpkg install protobuf:x86-uwp
-vcpkg install asio:x86-uwp
-vcpkg install opencv4[core]:x86-uwp
-vcpkg install apriltag:x86-uwp
-vcpkg install eigen3:x86-uwp
-vcpkg install foonathan-memory:x86-uwp
-vcpkg install poco:x86-uwp
-
 cd tools
 call colcon build --merge-install --cmake-args -DBUILD_TESTING=OFF
 cd ..
 
+goto :eol
+
+:novcpkg
+echo "VCPkg not found at c:\opt\vcpkg\vcpkg.exe"
+dir c:\opt
+
+exit /1
