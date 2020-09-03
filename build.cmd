917c12a852a1d7335ba86a3ec6c9abd3a29c27dd
Lou Amadio <lamadio@microsoft.com>
2020-09-02 17:24:41 -0700

    Breakup nuget, fix pipeline

--- build.cmd
+++ build.cmd
@@ -1,5 +1,15 @@
 @echo off
-setlocal enableextensions
+setlocal enableextensions disabledelayedexpansion
+
+:: Parse options
+:GETOPTS
+ if /I "%~1" == "/?" goto USAGE
+ if /I "%~1" == "/Help" goto USAGE
+ if /I "%~1" == "/x86" set BUILD=x86
+ if /I "%~1" == "/arm64" set BUILD=arm64
+ shift
+if not (%1)==() goto GETOPTS
+
 
 : Call to build the isolated ROS2 build system
 set ChocolateyInstall=c:\opt\chocolatey
@@ -10,6 +20,9 @@ set PATH_ORIG=%PATH%
 
 set VCPKG_ROOT=c:\opt\vcpkg
 set PYTHONHOME=C:\opt\python37amd64\
+
+if "%BUILD%"=="x86" goto :build_x86
+if "%BUILD%"=="arm64" goto :build_arm64
 
 set CMAKE_PREFIX_PATH=C:/opt/vcpkg/installed/x64-uwp;%CMAKE_PREFIX_PATH_ORIG%
 set PATH=c:\opt\vcpkg;c:\opt\vcpkg\installed\x64-uwp\bin;%PATH_ORIG%
@@ -26,7 +39,7 @@ ren install x64
 popd
 
 :build_arm64
-set ROS2_ARCH=x64
+set ROS2_ARCH=arm64
 
 set CMAKE_PREFIX_PATH=C:/opt/vcpkg/installed/arm64-uwp;%CMAKE_PREFIX_PATH_ORIG%
 set PATH=c:\opt\vcpkg;c:\opt\vcpkg\installed\arm64-uwp\bin;%PATH_ORIG%
@@ -36,6 +49,7 @@ rd /s /q build
 call colcon build --merge-install --packages-ignore tf2_py examples_tf2_py rmw_fastrtps_dynamic_cpp rcl_logging_log4cxx rcl_logging_spdlog rclcpp_components ros2trace tracetools_launch tracetools_read tracetools_test tracetools_trace rcldotnet_examples --cmake-args -A %ROS2_ARCH% -DDOTNET_CORE_ARCH=anycpu -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DINSTALL_EXAMPLES=OFF -DBUILD_TESTING=OFF -DRCL_LOGGING_IMPLEMENTATION=rcl_logging_noop --no-warn-unused-cli  -DCMAKE_BUILD_TYPE=RelWithDebInfo
 ren install arm64
 popd
+if "%BUILD%"=="arm64" goto :eof
 
 : build x86
 :build_x86
@@ -50,3 +64,10 @@ rd /s /q build
 call colcon build --merge-install --packages-ignore tf2_py examples_tf2_py rmw_fastrtps_dynamic_cpp rcl_logging_log4cxx rcl_logging_spdlog rclcpp_components ros2trace tracetools_launch tracetools_read tracetools_test tracetools_trace rcldotnet_examples --cmake-args -A %ROS2_ARCH% -DDOTNET_CORE_ARCH=anycpu -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DTHIRDPARTY=ON -DINSTALL_EXAMPLES=OFF -DBUILD_TESTING=OFF -DRCL_LOGGING_IMPLEMENTATION=rcl_logging_noop --no-warn-unused-cli  -DCMAKE_BUILD_TYPE=RelWithDebInfo
 ren install x86
 popd
+if "%BUILD%"=="x86" goto :eof
+
+goto :eof
+
+:USAGE
+    echo build [x86|arm64]
+    goto :eof
--- initvcpkg.cmd
+++ initvcpkg.cmd
@@ -1,6 +1,15 @@
 @echo off
 setlocal enableextensions disabledelayedexpansion
 
+:: Parse options
+:GETOPTS
+ if /I "%~1" == "/?" goto USAGE
+ if /I "%~1" == "/Help" goto USAGE
+ if /I "%~1" == "/x86" set BUILD=x86
+ if /I "%~1" == "/arm64" set BUILD=arm64
+ shift
+if not (%1)==() goto GETOPTS
+
 set PATH_ORIG=%PATH%
 set PATH=c:\opt\vcpkg;c:\opt\chocolatey\bin;C:\opt\python37amd64\;C:\opt\python37amd64\Scripts;C:\opt\python37amd64\DLLs;%PATH%
 set VCPKG_ROOT=c:\opt\vcpkg
@@ -11,26 +20,30 @@ git clone https://github.com/ooeygui/vcpkg
 call bootstrap-vcpkg.bat
 popd
 
-
 : Build tooling
 vcpkg install protobuf:x86-windows
 vcpkg install foonathan-memory:x86-windows
+
+if "%BUILD%"=="x86" goto :build_x86
+if "%BUILD%"=="arm64" goto :build_arm64
+
 
 :build_arm64
 vcpkg install protobuf:arm64-uwp
 vcpkg install asio:arm64-uwp
 vcpkg install tinyxml2:arm64-uwp
 vcpkg install opencv4[core]:arm64-uwp
-vcpkg install apriltag:arm64-uwp
+vcpkg install apriltag[core]:arm64-uwp
 vcpkg install eigen3:arm64-uwp
 vcpkg install foonathan-memory[core]:arm64-uwp
 vcpkg install poco:arm64-uwp
+if "%BUILD%"=="arm64" goto :eof
 
 :build_x64
 vcpkg install protobuf:x64-uwp
 vcpkg install asio:x64-uwp
 vcpkg install opencv4[core]:x64-uwp
-vcpkg install apriltag:x64-uwp
+vcpkg install apriltag[core]:x64-uwp
 vcpkg install eigen3:x64-uwp
 vcpkg install foonathan-memory[core]:x64-uwp
 vcpkg install poco:x64-uwp
@@ -39,7 +52,7 @@ vcpkg install poco:x64-uwp
 vcpkg install protobuf:x86-uwp
 vcpkg install asio:x86-uwp
 vcpkg install opencv4[core]:x86-uwp
-vcpkg install apriltag:x86-uwp
+vcpkg install apriltag[core]:x86-uwp
 vcpkg install eigen3:x86-uwp
 vcpkg install foonathan-memory:x86-uwp
 vcpkg install poco:x86-uwp
